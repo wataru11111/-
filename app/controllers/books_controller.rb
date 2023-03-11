b27b2832e97662f836d525499881bd1303bcf233
EC2 Default User <ec2-user@ip-172-31-42-22.ap-northeast-1.compute.internal>
2023-03-11 12:28:10 +0000

    Theme3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,53 +1,44 @@
 class BooksController < ApplicationController
- def new
-
- end
 
  def create
-     @model_class_name = ModelClassName.new(params[:model_class_name])
-     if @model_class_name.save
-             flash[:notice] = 'ModelClassName was successfully created.'
-     respond_to do |wants|
-
-         else
-
-         end
-     end
+   @book = Book.new(books_params)
+   if @book.save
+   flash[:notice] = "Book was successfully created."
+   redirect_to book_path(@book.id)
+   else
+    @books = Book.all
+       render :index
+   end
  end
 
  def index
-     @model_class_name = ModelClassName.all
-
+   @book = Book.new
+   @books = Book.all
  end
 
  def show
-     @model_class_name = ModelClassName.find(params[:id])
-
+   @book = Book.find(params[:id])
  end
 
  def edit
-     @model_class_name = ModelClassName.find(params[:id])
+     @book = Book.find(params[:id])
  end
 
  def update
-     @model_class_name = ModelClassName.find(params[:id])
-             wants.html { redirect_to(@model_class_name) }
-             wants.xml  { head :ok }
-              respond_to do |wants|
-     end
+    book = Book.find(params[:id])
+    book.update(books_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(book.id)
  end
  def destroy
-     @model_class_name = ModelClassName.find(params[:id])
-     @model_class_name.destroy
-
-     respond_to do |wants|
+     book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
  end
-
-end
 
 private
 
   def books_params
-    params.require(:list).permit(:title, :body, :image)
+    params.require(:book).permit(:title, :body,)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show', as: :book
+  get 'books/:id/edit' => 'books#edit', as: :edit
   get '/top' => 'homes#top'
+  post 'books' => 'books#create'
+  patch 'books/:id' => 'books#update', as: :update_book
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
